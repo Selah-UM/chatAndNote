20ef493c38a7b7c48cc9c6518d04ba19c77bcbb8
selah2 <konomi_20n1101220@nnn.ed.jp>
2022-09-07 14:12:55 +0900

    ログイン時と未ログ イン時で区別no実装 #

--- test/test.js
+++ test/test.js
@@ -4,6 +4,41 @@ const app = require('../app');
 const passportStub = require('passport-stub');
 // const app = require('../app');
 
+
+describe('index', () => {
+    beforeAll(() => {
+        passportStub.install(app);
+        passportStub.login({ username: 'testuser' });
+    });
+    afterAll(() => {
+        passportStub.logout();
+        passportStub.uninstall();
+    });
+
+    test('ログイン時はログアウトのためのリンクが含まれる', async () => {
+        await request(app)
+            .get('/')
+            .expect('Content-Type', 'text/html; charset=utf-8')
+            .expect(/<a href="\/logout"/)
+            .expect(200)
+    });
+    test('ログイン時はユーザー名が表示される', async () => {
+        await request(app)
+            .get('/')
+            .expect(/testuser/)
+            .expect(200)
+    });
+});
+describe('index', () => {
+    test('ログアウト時はログインのためのリンクが含まれる', async () => {
+        await request(app)
+            .get('/')
+            .expect('Content-Type', 'text/html; charset=utf-8')
+            .expect(/<a href="\/login"/)
+            .expect(200)
+    });
+});
+
 describe('login', () => {
     beforeAll(() => {
         passportStub.install(app);
@@ -14,19 +49,13 @@ describe('login', () => {
         passportStub.uninstall();
     });
 
-    test('ログインのためのリンクが含まれる', async () => {
+    test('GitHubログインのためのリンクが含まれる', async () => {
         await request(app)
             .get('/login')
             .expect('Content-Type', 'text/html; charset=utf-8')
             .expect(/<a href="\/auth\/github"/)
             .expect(200)
     });
-    test('ログイン時はユーザー名が表示される', async () => {
-        await request(app)
-            .get('/login')
-            .expect(/testuser/)
-            .expect(200)
-    })
 });
 
 describe('logout', () => {
