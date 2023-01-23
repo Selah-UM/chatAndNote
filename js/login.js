add2a0dbaec6d64d32cbc079e39da24be7d530e8
selah2 <konomi_20n1101220@nnn.ed.jp>
2023-01-23 13:36:19 +0900

    try the signInWithRedirect #22

--- js/login.js
+++ js/login.js
@@ -1,49 +1,49 @@
 'use strict';
 console.log("login.js");
 
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
+// import firebase from "firebase/compat/app";
+// import "firebase/compat/auth";
 // var firebase = require('firebase');
-var firebaseui = require('firebaseui');
+// var firebaseui = require('firebaseui');
 
-const firebaseConfig = {
-  apiKey: "AIzaSyC1cL-sLI_5tTvHh7_wX1vFCgm3NIHh_QI",
-  authDomain: "nonchansecondapp.firebaseapp.com",
-  databaseURL: "https://nonchansecondapp.firebaseio.com",
-  projectId: "nonchansecondapp",
-  storageBucket: "nonchansecondapp.appspot.com",
-  messagingSenderId: "236461473194",
-  appId: "1:236461473194:web:e0ec1a6fd3b7a37eea3fd3",
-  measurementId: "G-W5PMDCS135"
-};
-firebase.initializeApp(firebaseConfig);
+// const firebaseConfig = {
+//   apiKey: "AIzaSyC1cL-sLI_5tTvHh7_wX1vFCgm3NIHh_QI",
+//   authDomain: "nonchansecondapp.firebaseapp.com",
+//   databaseURL: "https://nonchansecondapp.firebaseio.com",
+//   projectId: "nonchansecondapp",
+//   storageBucket: "nonchansecondapp.appspot.com",
+//   messagingSenderId: "236461473194",
+//   appId: "1:236461473194:web:e0ec1a6fd3b7a37eea3fd3",
+//   measurementId: "G-W5PMDCS135"
+// };
+// firebase.initializeApp(firebaseConfig);
 
 // const auth = firebase.auth();
 // Initialize the FirebaseUI Widget using Firebase.
 // var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-let uiConfig = {
-  callbacks: {
-      signInSuccessWithAuthResult: function (authResult, redirectUrl) {
-          console.log("ログイン成功");
-          console.log(authResult.user.uid);
-          // document.cookie = `uid=${authResult.user.uid}`;
-          const user = firebase.auth().currentUser;
-          console.log('login.js user = ' + user);
-          // window.location.assign(`/index`);
+// let uiConfig = {
+//   callbacks: {
+//       signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+//           console.log("ログイン成功");
+//           console.log(authResult.user.uid);
+//           // document.cookie = `uid=${authResult.user.uid}`;
+//           const user = firebase.auth().currentUser;
+//           console.log('login.js user = ' + user);
+//           // window.location.assign(`/index`);
 
-          return true;
-      },
-  },
-  // signInFlow: 'popup',
-  signInSuccessUrl: '/index',
-  signInOptions: [
-    firebase.auth.GithubAuthProvider.PROVIDER_ID
-  ],
-};
+//           return true;
+//       },
+//   },
+//   // signInFlow: 'popup',
+//   signInSuccessUrl: '/index',
+//   signInOptions: [
+//     firebase.auth.GithubAuthProvider.PROVIDER_ID
+//   ],
+// };
 
-let ui = new firebaseui.auth.AuthUI(firebase.auth());
-ui.start('#firebaseui-auth-container', uiConfig);
+// let ui = new firebaseui.auth.AuthUI(firebase.auth());
+// ui.start('#firebaseui-auth-container', uiConfig);
 
 // const button = document.getElementById('logout');
 // button.addEventListener('click', () => {
@@ -56,51 +56,51 @@ ui.start('#firebaseui-auth-container', uiConfig);
 // });
 
 
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 
-// import { getAuth, getRedirectResult, signInWithRedirect, signInWithPopup, GithubAuthProvider, onAuthStateChanged } from "firebase/auth";
+import { getAuth, getRedirectResult, signInWithRedirect, signInWithPopup, GithubAuthProvider, onAuthStateChanged } from "firebase/auth";
 
-// const firebaseConfig = {
-//     apiKey: "AIzaSyATXy5h6R5jFoy1gEppzBXABhwfxfCOEYc",
-//     authDomain: "chatandnote.firebaseapp.com",
-//     databaseURL: "https://chatandnote-default-rtdb.firebaseio.com/",
-//     projectId: "chatandnote",
-//     storageBucket: "chatandnote.appspot.com",
-//     messagingSenderId: "1076201869761",
-//     appId: "1:1076201869761:web:b4b81fec24525fead7df40",
-//     measurementId: "G-CFWX5TT79V"
-// }
-// const app = initializeApp(firebaseConfig);
+const firebaseConfig = {
+    apiKey: "AIzaSyATXy5h6R5jFoy1gEppzBXABhwfxfCOEYc",
+    authDomain: "chatandnote.firebaseapp.com",
+    databaseURL: "https://chatandnote-default-rtdb.firebaseio.com/",
+    projectId: "chatandnote",
+    storageBucket: "chatandnote.appspot.com",
+    messagingSenderId: "1076201869761",
+    appId: "1:1076201869761:web:b4b81fec24525fead7df40",
+    measurementId: "G-CFWX5TT79V"
+}
+const app = initializeApp(firebaseConfig);
 
-// const provider = new GithubAuthProvider();
+const provider = new GithubAuthProvider();
 
-// // const auth = getAuth();
+// const auth = getAuth();
 
-// signInWithRedirect(auth, provider);
+signInWithRedirect(auth, provider);
 
-// getRedirectResult(auth)
-//   .then((result) => {
-//     const credential = GithubAuthProvider.credentialFromResult(result);
-//     if (credential) {
-//       // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-//       const token = credential.accessToken;
-//       // ...
-//     }
+getRedirectResult(auth)
+  .then((result) => {
+    const credential = GithubAuthProvider.credentialFromResult(result);
+    if (credential) {
+      // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+      const token = credential.accessToken;
+      // ...
+    }
 
-//     // The signed-in user info.
-//     const user = result.user;
-//     console.log(user);
-//   }).catch((error) => {
-//     // Handle Errors here.
-//     const errorCode = error.code;
-//     const errorMessage = error.message;
-//     // The email of the user's account used.
-//     const email = error.customData.email;
-//     // The AuthCredential type that was used.
-//     const credential = GithubAuthProvider.credentialFromError(error);
-//     // ...
-//   });
+    // The signed-in user info.
+    const user = result.user;
+    console.log(user);
+  }).catch((error) => {
+    // Handle Errors here.
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // The email of the user's account used.
+    const email = error.customData.email;
+    // The AuthCredential type that was used.
+    const credential = GithubAuthProvider.credentialFromError(error);
+    // ...
+  });
 
 
 // // signInWithPopup(auth, provider)
