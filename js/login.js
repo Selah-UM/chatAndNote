2d83406b9ee5d464d32de7754d39384b1940c38f
selah2 <konomi_20n1101220@nnn.ed.jp>
2023-01-23 14:28:54 +0900

    try the signInWithRedirect3 #22

--- js/login.js
+++ js/login.js
@@ -79,28 +79,28 @@ const auth = getAuth();
 
 signInWithRedirect(auth, provider);
 
-getRedirectResult(auth)
-  .then((result) => {
-    const credential = GithubAuthProvider.credentialFromResult(result);
-    if (credential) {
-      // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-      const token = credential.accessToken;
-      // ...
-    }
+// getRedirectResult(auth)
+//   .then((result) => {
+//     const credential = GithubAuthProvider.credentialFromResult(result);
+//     if (credential) {
+//       // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+//       const token = credential.accessToken;
+//       // ...
+//     }
 
-    // The signed-in user info.
-    const user = result.user;
-    console.log(user);
-  }).catch((error) => {
-    // Handle Errors here.
-    const errorCode = error.code;
-    const errorMessage = error.message;
-    // The email of the user's account used.
-    const email = error.customData.email;
-    // The AuthCredential type that was used.
-    const credential = GithubAuthProvider.credentialFromError(error);
-    // ...
-  });
+//     // The signed-in user info.
+//     const user = result.user;
+//     console.log(user);
+//   }).catch((error) => {
+//     // Handle Errors here.
+//     const errorCode = error.code;
+//     const errorMessage = error.message;
+//     // The email of the user's account used.
+//     const email = error.customData.email;
+//     // The AuthCredential type that was used.
+//     const credential = GithubAuthProvider.credentialFromError(error);
+//     // ...
+//   });
 
 
 // // signInWithPopup(auth, provider)
